02e2ae4f4b8cdd52cf46e1df548f8eb6c5b8ac2f
Yassine <yassinetech0@gmail.com>
2026-07-13 16:20:19 +0200

    wip

---
+++ resources/views/testlogin.blade.php
@@ -0,0 +1,801 @@
+<div class="sign-in">
+  <div class="frame">
+    <div class="background">
+      <div class="medical-cross-decorations-"><span class="medicalcrossdecorations_span">✚</span></div>
+      <div class="medical-cross"><span class="medicalcross_span">✚</span></div>
+      <div class="medical-cross_01"><span class="medicalcross_01_span">✚</span></div>
+      <div class="animated-particles">
+        <div class="overlay"></div>
+        <div class="overlay_01"></div>
+        <div class="overlay_02"></div>
+        <div class="overlay_03"></div>
+        <div class="overlay_04"></div>
+        <div class="overlay_05"></div>
+        <div class="overlay_06"></div>
+        <div class="overlay_07"></div>
+        <div class="overlay_08"></div>
+        <div class="overlay_09"></div>
+        <div class="overlay_10"></div>
+        <div class="overlay_11"></div>
+        <div class="overlay_12"></div>
+        <div class="overlay_13"></div>
+        <div class="overlay_14"></div>
+        <div class="overlay_15"></div>
+        <div class="overlay_16"></div>
+        <div class="overlay_17"></div>
+        <div class="overlay_18"></div>
+        <div class="overlay_19"></div>
+        <div class="overlay_20"></div>
+        <div class="overlay_21"></div>
+      </div>
+      <div class="brand-left">
+        <div class="product-name"><span class="productname_span_01">VSahha<br/></span><span class="productname_span_02">International Health Solutions</span></div>
+        <div class="heading-1-the-future-of-health-insurance-management"><span class="heading1thefutureofhealthinsurancemanagement_span_01">The Future of<br/></span><span class="heading1thefutureofhealthinsurancemanagement_span_02">Health Insurance<br/></span><span class="heading1thefutureofhealthinsurancemanagement_span_03">Management</span></div>
+        <div class="product-description"><span class="productdescription_span">A complete CRM platform for managing insurance<br/>policies, partners, members, and coverage —<br/>built for modern health operations.</span></div>
+      </div>
+      <div class="login-form-right">
+        <div class="heading-2-welcome-back-"><span class="heading2welcomeback_span">Welcome </span></div>
+        <div class="dashboard-description"><span class="dashboarddescription_span">Sign in to your VSahha dashboard</span></div>
+        <div class="label-email-address"><span class="labelemailaddress_span">Email address</span></div>
+        <div class="input">
+          <div class="container">
+            <div class="email-address"><span class="emailaddress_span">admin@vsahha.com</span></div>
+          </div>
+        </div>
+        <div class="svg">
+          <div class="vector"></div>
+          <div class="vector_01"></div>
+        </div>
+        <div class="label-password"><span class="labelpassword_span">Password</span></div>
+        <div class="input_01">
+          <div class="container_01">
+            <div class="username"><span class="username_span">vsahha2026</span></div>
+          </div>
+        </div>
+        <div class="svg_01">
+          <div class="vector_02"></div>
+          <div class="vector_03"></div>
+        </div>
+        <div class="input_02"></div>
+        <div class="label-remember-me"><span class="labelrememberme_span">Remember me</span></div>
+        <div class="link-forgot-password"><span class="linkforgotpassword_span">Forgot password?</span></div>
+        <div class="button">
+          <div class="button-label"><span class="buttonlabel_span">Sign in to Dashboard</span></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+.medicalcrossdecorations_span {
+  color: white;
+  font-size: 120px;
+  font-family: Inter;
+  font-weight: 300;
+  line-height: 120px;
+  word-wrap: break-word;
+}
+
+.medical-cross-decorations- {
+  width: 90.35px;
+  height: 108px;
+  left: 94.90px;
+  top: 86.40px;
+  position: absolute;
+  opacity: 0.05;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.medicalcross_span {
+  color: white;
+  font-size: 60px;
+  font-family: Inter;
+  font-weight: 300;
+  line-height: 60px;
+  word-wrap: break-word;
+}
+
+.medical-cross {
+  width: 45.29px;
+  height: 54px;
+  left: 37.95px;
+  top: 648px;
+  position: absolute;
+  opacity: 0.05;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.medicalcross_01_span {
+  color: white;
+  font-size: 80px;
+  font-family: Inter;
+  font-weight: 300;
+  line-height: 80px;
+  word-wrap: break-word;
+}
+
+.medical-cross_01 {
+  width: 60.31px;
+  height: 72px;
+  left: 1780.95px;
+  top: 216px;
+  position: absolute;
+  opacity: 0.05;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.overlay {
+  width: 14.97px;
+  height: 14.97px;
+  left: 1517.33px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 7.49px;
+}
+
+.overlay_01 {
+  width: 26.44px;
+  height: 26.44px;
+  left: 354.97px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 13.22px;
+}
+
+.overlay_02 {
+  width: 33.28px;
+  height: 33.28px;
+  left: 1063.36px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 16.64px;
+}
+
+.overlay_03 {
+  width: 59.84px;
+  height: 59.84px;
+  left: 196.98px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 29.92px;
+}
+
+.overlay_04 {
+  width: 15.72px;
+  height: 15.72px;
+  left: 41.11px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 7.86px;
+}
+
+.overlay_05 {
+  width: 44.98px;
+  height: 44.98px;
+  left: 541.89px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 22.49px;
+}
+
+.overlay_06 {
+  width: 51.66px;
+  height: 51.66px;
+  left: 822.88px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 25.83px;
+}
+
+.overlay_07 {
+  width: 36.81px;
+  height: 36.81px;
+  left: 1143.48px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 18.41px;
+}
+
+.overlay_08 {
+  width: 68.13px;
+  height: 68.13px;
+  left: 1890.56px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 34.07px;
+}
+
+.overlay_09 {
+  width: 52.55px;
+  height: 52.55px;
+  left: 779.50px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 26.28px;
+}
+
+.overlay_10 {
+  width: 10.58px;
+  height: 10.58px;
+  left: 1856.56px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 5.29px;
+}
+
+.overlay_11 {
+  width: 30.14px;
+  height: 30.14px;
+  left: 129.38px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 15.07px;
+}
+
+.overlay_12 {
+  width: 20px;
+  height: 20px;
+  left: 1082.16px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 10px;
+}
+
+.overlay_13 {
+  width: 51.72px;
+  height: 51.72px;
+  left: 7.50px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 25.86px;
+}
+
+.overlay_14 {
+  width: 11.25px;
+  height: 11.25px;
+  left: 1198.30px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 5.63px;
+}
+
+.overlay_15 {
+  width: 15.59px;
+  height: 15.59px;
+  left: 127.84px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 7.80px;
+}
+
+.overlay_16 {
+  width: 36.27px;
+  height: 36.27px;
+  left: 247.09px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 18.14px;
+}
+
+.overlay_17 {
+  width: 23.39px;
+  height: 23.39px;
+  left: 689.38px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 11.70px;
+}
+
+.overlay_18 {
+  width: 25.38px;
+  height: 25.38px;
+  left: 1823.20px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 12.69px;
+}
+
+.overlay_19 {
+  width: 68.44px;
+  height: 68.44px;
+  left: 1025.03px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 34.22px;
+}
+
+.overlay_20 {
+  width: 14.95px;
+  height: 14.95px;
+  left: 1000.98px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 7.48px;
+}
+
+.overlay_21 {
+  width: 16.88px;
+  height: 16.88px;
+  left: 1666.53px;
+  top: 0px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.06);
+  border-radius: 8.44px;
+}
+
+.productname_span_01 {
+  color: white;
+  font-size: 40px;
+  font-family: Inter;
+  font-weight: 800;
+  word-wrap: break-word;
+}
+
+.productname_span_02 {
+  color: white;
+  font-size: 24px;
+  font-family: Inter;
+  font-weight: 400;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  word-wrap: break-word;
+}
+
+.product-name {
+  width: 531px;
+  height: 45px;
+  left: 304px;
+  top: 332px;
+  position: absolute;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.heading1thefutureofhealthinsurancemanagement_span_01 {
+  color: white;
+  font-size: 48px;
+  font-family: Inter;
+  font-weight: 700;
+  line-height: 48.30px;
+  word-wrap: break-word;
+}
+
+.heading1thefutureofhealthinsurancemanagement_span_02 {
+  color: #26C6A0;
+  font-size: 48px;
+  font-family: Inter;
+  font-weight: 700;
+  line-height: 48.30px;
+  word-wrap: break-word;
+}
+
+.heading1thefutureofhealthinsurancemanagement_span_03 {
+  color: white;
+  font-size: 48px;
+  font-family: Inter;
+  font-weight: 700;
+  line-height: 48.30px;
+  word-wrap: break-word;
+}
+
+.heading-1-the-future-of-health-insurance-management {
+  width: 646px;
+  height: 147px;
+  left: 304px;
+  top: 440px;
+  position: absolute;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.productdescription_span {
+  color: rgba(255, 255, 255, 0.55);
+  font-size: 24px;
+  font-family: Inter;
+  font-weight: 400;
+  line-height: 35px;
+  word-wrap: break-word;
+}
+
+.product-description {
+  width: 953px;
+  height: 74px;
+  left: 304px;
+  top: 630px;
+  position: absolute;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.heading2welcomeback_span {
+  color: #2D3748;
+  font-size: 26px;
+  font-family: Inter;
+  font-weight: 700;
+  word-wrap: break-word;
+}
+
+.heading-2-welcome-back- {
+  width: 196.62px;
+  height: 31px;
+  left: -3.50px;
+  top: 35.50px;
+  position: absolute;
+  text-align: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.dashboarddescription_span {
+  color: #4A5568;
+  font-size: 14px;
+  font-family: Inter;
+  font-weight: 400;
+  word-wrap: break-word;
+}
+
+.dashboard-description {
+  width: 245px;
+  height: 17px;
+  left: 38px;
+  top: 78px;
+  position: absolute;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.labelemailaddress_span {
+  color: #2D3748;
+  font-size: 13px;
+  font-family: Inter;
+  font-weight: 600;
+  letter-spacing: 0.30px;
+  word-wrap: break-word;
+}
+
+.label-email-address {
+  width: 105px;
+  height: 16px;
+  left: 48px;
+  top: 139px;
+  position: absolute;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.emailaddress_span {
+  color: rgba(45, 55, 72, 0.42);
+  font-size: 15px;
+  font-family: Inter;
+  font-weight: 400;
+  word-wrap: break-word;
+}
+
+.email-address {
+  width: 144px;
+  height: 19px;
+  left: 0px;
+  top: 0px;
+  position: absolute;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.vector {
+  width: 15px;
+  height: 12px;
+  left: 1.50px;
+  top: 3px;
+  position: absolute;
+  outline: 1.50px #9CA3AF solid;
+  outline-offset: -0.75px;
+}
+
+.vector_01 {
+  width: 15px;
+  height: 5.25px;
+  left: 1.50px;
+  top: 4.50px;
+  position: absolute;
+  outline: 1.50px #9CA3AF solid;
+  outline-offset: -0.75px;
+}
+
+.labelpassword_span {
+  color: #2D3748;
+  font-size: 13px;
+  font-family: Inter;
+  font-weight: 600;
+  letter-spacing: 0.30px;
+  word-wrap: break-word;
+}
+
+.label-password {
+  width: 74px;
+  height: 16px;
+  left: 48px;
+  top: 230px;
+  position: absolute;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.username_span {
+  color: rgba(45, 55, 72, 0.40);
+  font-size: 15px;
+  font-family: Inter;
+  font-weight: 400;
+  word-wrap: break-word;
+}
+
+.username {
+  width: 87.84px;
+  height: 19px;
+  left: 0px;
+  top: 0px;
+  position: absolute;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.vector_02 {
+  width: 13.50px;
+  height: 8.25px;
+  left: 2.25px;
+  top: 8.25px;
+  position: absolute;
+  outline: 1.50px #9CA3AF solid;
+  outline-offset: -0.75px;
+}
+
+.vector_03 {
+  width: 7.50px;
+  height: 6.75px;
+  left: 5.25px;
+  top: 1.50px;
+  position: absolute;
+  outline: 1.50px #9CA3AF solid;
+  outline-offset: -0.75px;
+}
+
+.input_02 {
+  width: 16px;
+  height: 16px;
+  left: 63px;
+  top: 337px;
+  position: absolute;
+  transform: rotate(180deg);
+  transform-origin: top left;
+  border-radius: 2.50px;
+  border: 1px #1461B6 solid;
+}
+
+.labelrememberme_span {
+  color: #4A5568;
+  font-size: 13px;
+  font-family: Inter;
+  font-weight: 400;
+  word-wrap: break-word;
+}
+
+.label-remember-me {
+  width: 94px;
+  height: 16px;
+  left: 72px;
+  top: 321px;
+  position: absolute;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.linkforgotpassword_span {
+  color: #0B4F8A;
+  font-size: 13px;
+  font-family: Inter;
+  font-weight: 500;
+  word-wrap: break-word;
+}
+
+.link-forgot-password {
+  width: 89.64px;
+  height: 16px;
+  left: 280.73px;
+  top: 321px;
+  position: absolute;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.buttonlabel_span {
+  color: white;
+  font-size: 15px;
+  font-family: Inter;
+  font-weight: 600;
+  letter-spacing: 0.30px;
+  word-wrap: break-word;
+}
+
+.button-label {
+  width: 156.40px;
+  height: 19px;
+  left: 82.95px;
+  top: 14px;
+  position: absolute;
+  text-align: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.brand-left {
+  width: 842px;
+  height: 1080px;
+  left: 0px;
+  top: 0px;
+  position: absolute;
+}
+
+.container {
+  width: 262px;
+  height: 19px;
+  left: 45px;
+  top: 14px;
+  position: absolute;
+  overflow: hidden;
+}
+
+.container_01 {
+  width: 262px;
+  height: 19px;
+  left: 45px;
+  top: 14px;
+  position: absolute;
+  overflow: hidden;
+}
+
+.button {
+  width: 322px;
+  height: 47px;
+  left: 48px;
+  top: 373px;
+  position: absolute;
+  background: linear-gradient(172deg, #0B4F8A 0%, #1565C0 100%);
+  box-shadow: 0px 4px 20px rgba(11, 79, 138, 0.35);
+  overflow: hidden;
+  border-radius: 12px;
+}
+
+.animated-particles {
+  width: 1885px;
+  height: 1080px;
+  left: 13px;
+  top: 0px;
+  position: absolute;
+}
+
+.svg {
+  width: 18px;
+  height: 18px;
+  left: 87px;
+  top: 197px;
+  position: absolute;
+  transform: rotate(180deg);
+  transform-origin: top left;
+  overflow: hidden;
+}
+
+.svg_01 {
+  width: 18px;
+  height: 18px;
+  left: 88px;
+  top: 286px;
+  position: absolute;
+  transform: rotate(180deg);
+  transform-origin: top left;
+  overflow: hidden;
+}
+
+.input {
+  width: 322px;
+  height: 47px;
+  left: 48px;
+  top: 163px;
+  position: absolute;
+  background: white;
+  overflow: hidden;
+  border-radius: 12px;
+  outline: 1px #E5E7EB solid;
+  outline-offset: -1px;
+}
+
+.input_01 {
+  width: 322px;
+  height: 47px;
+  left: 48px;
+  top: 254px;
+  position: absolute;
+  background: white;
+  overflow: hidden;
+  border-radius: 12px;
+  outline: 1px #E5E7EB solid;
+  outline-offset: -1px;
+}
+
+.login-form-right {
+  width: 418px;
+  height: 445px;
+  left: 1064px;
+  top: 291px;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.97);
+  box-shadow: 0px 24px 80px rgba(0, 0, 0, 0.25);
+  border-radius: 28px;
+  backdrop-filter: blur(10px);
+}
+
+.background {
+  width: 1898px;
+  height: 1080px;
+  left: 0px;
+  top: 0px;
+  position: absolute;
+  background: linear-gradient(0deg, white 0%, white 100%), linear-gradient(148deg, #0B4F8A 0%, #0D3D6E 50%, #062A55 100%);
+  background-blend-mode: multiply, normal;
+  overflow: hidden;
+}
+
+.frame {
+  width: 1898px;
+  height: 1073px;
+  left: 0px;
+  top: 0px;
+  position: absolute;
+  background: linear-gradient(0deg, #F8FAFB 0%, #F8FAFB 100%), white;
+}
+
+.sign-in {
+  width: 1898px;
+  height: 1080px;
+  position: relative;
+  background: #444444;
+}
+</style>
